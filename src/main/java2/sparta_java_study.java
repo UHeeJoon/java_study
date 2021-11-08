f8a09109a6806769fb9d0c675d4cdd18107549b0
UHeeJoon <yhj3855@naver.com>
2021-11-08 19:27:50 +0900

    강의 정리

---
+++ src/main/java2/sparta_java_study.java
@@ -0,0 +1,30 @@
+//public class sparta_java_study {
+//        public static void main(String[] args) {
+//            // 자료형
+//            // short, int, long, double, char, String, boolean, byte는 아스키코드 출력
+//            // Short.MAX_VALUE, .MIN_VALUE 라고 하면 해당 자료형이 나타낼 수 있는 최대, 최솟값 출력
+//
+//            // 상수는  자료형 앞에 final을 붙인다. final을 다른 말로 접근제어자 라고 한다
+//            // final int
+//            // final double
+//            // final String
+//
+//            // 배열
+//            // int[] intArray = new int[] {1, 2, 3, 4, 5};
+//            // String[] a = {""}
+//            // 배열 초기화 안해주면 int는 0, String은 null 값을 가짐
+//
+//            // +=, -=, *=, /=, %= 사용 가능
+//            // &&, || 사용
+//
+//            // 조건문
+//            // if , else if, else | break, continue
+//            // switch(a){
+//                // case 1: case 2: case 3: case 4: ~~ break;
+//                // default: ~~;    }
+//
+//            // 삼항 연산자
+//            // a>b? a=1: b=1;
+//
+//        }
+//}
